8a6aeb6d7bdcc47ae22449dc54719b23f9ef842d
djhugomartis2018-a11y <djhugomartis2018@gmail.com>
2026-06-24 09:29:19 -0300

    fix: center text element by updating left and top positioning to 50%

--- src/imports/Group272-1/index.tsx
+++ src/imports/Group272-1/index.tsx
@@ -2,7 +2,7 @@ export default function Group() {
   return (
     <div className="contents relative size-full">
       <div className="absolute bg-[#a0a320] border border-[#c5c5c5] border-solid h-[31px] left-0 rounded-[20px] top-0 w-[157px]" />
-      <div className="-translate-x-1/2 -translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Gotham:Book',sans-serif] justify-center leading-[0] left-[62px] not-italic text-[#39471d] text-[14px] text-center top-[10.5px] whitespace-nowrap">
+      <div className="-translate-x-1/2 -translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Gotham:Book',sans-serif] justify-center leading-[0] left-1/2 not-italic text-[#39471d] text-[14px] text-center top-1/2 whitespace-nowrap">
         <p className="leading-[21px]">entre 25 a 35 dias</p>
       </div>
     </div>
